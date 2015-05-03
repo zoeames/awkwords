94c9261a7a459bb0a5bab05a03c30f9bfbbd4868
Zoe Ames <zoeames@gmail.com>
2015-05-03 13:51:29 -0500

    routing fixed

--- client/index.js
+++ client/index.js
@@ -7,6 +7,8 @@
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
     .when('/about', {templateUrl:'/views/about/about.html', controller:'AboutCtrl'})
     .when('/game',    {templateUrl:'/views/game/game.html',       controller:'GameCtrl'})
+    .when('/newgame',    {templateUrl:'/views/newgame/newgame.html',       controller:'NewGameCtrl'})
+    .when('/results',    {templateUrl:'/views/results/results.html',       controller:'ResultsCtrl'})
     .otherwise({redirectTo:'/'});
 
     $httpProvider.interceptors.push('HttpInterceptor');
---
+++ client/views/results/results.js
@@ -0,0 +1,9 @@
+(function(){
+  'use strict';
+
+  angular.module('capstone')
+  .controller('ResultsCtrl', ['$scope', function($scope){
+
+  }]);
+})();
+
