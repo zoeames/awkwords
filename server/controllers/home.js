461be9330a8c0becc67e59de6f50e09a586e4fe0
Zoe Ames <zoeames@gmail.com>
2015-05-03 12:24:51 -0500

    mongo stripped out

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
 exports.index = function(req, res){
-  res.send({mean:['MongoDB', 'Express.js', 'Angular.js', 'Node.js']});
+  res.send({mean:['Express.js', 'Angular.js', 'Node.js']});
 };
 
